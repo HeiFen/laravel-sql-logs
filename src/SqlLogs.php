c9b10c4bb0ff0967f7f83a8fe5f3e7b401b25c47
刘晨雨 <liuchenyu@shall-buy.com>
2022-08-27 09:14:31 +0800

    fix

--- src/SqlLogs.php
+++ src/SqlLogs.php
@@ -2,20 +2,34 @@
 
 namespace HeiFen\LaravelSqlLogs;
 
-use Closure;
-use Illuminate\Support\Facades\Log;
-
 class SqlLogs
 {
-    public function handle($request, Closure $next)
+    public function handle($request, \Closure $next)
     {
         \DB::connection()->enableQueryLog();
+        
+        $response = $next($request);
+
         // 获取sql日志信息
         $logs = \DB::getQueryLog();
+        // dd($request->path());
 
         // 输出日志到文件
-        Log::info($logs);
+        \Log::info('——————————————————————————————————————————请求开始');
+        \Log::info('请求地址【' . $request->path() . '】');
+        foreach ($logs as $log) {
+            // 组装sql
+            $sql = $log['query'];
+            foreach ($log['bindings'] as $replace){
+                $value = is_numeric($replace) ? $replace : "'".$replace."'";
+                $sql = preg_replace('/\?/', $value, $sql, 1);
+            }
 
-        return $next($request);
+            \Log::info($sql);
+            \Log::info("耗时【 ".$log['time']." ms 】");
+        }
+        \Log::info('——————————————————————————————————————————请求结束');
+
+        return $response;
     }
 }
